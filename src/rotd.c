6815b2300de3bdfe62b7607ef4d34866c52efd9b
Varoun. P <contact@varoun.com>
2015-09-11 06:34:59 -0400

    Added some code to handle gethostbyname, it does not work fully, and does not segfault.

--- src/rotd.c
+++ src/rotd.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <nss.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 /*
 NOTE(varoun): Look at include/netdb.h in the glibc source for the
@@ -33,6 +35,30 @@ _nss_rotd_gethostbyname2_r (const char *name, int af, struct hostent *host,
                             char *buffer, size_t buflen,
                             int *errnop, int *h_errnop) {
   printf("@ %s\n", __FUNCTION__);
+  printf("ARG(name) -> %s\n", name);
+  printf("ARG(buflen) -> %d\n", buflen);
+
+  if (strcmp(name, "nsstest.varoun.com") == 0)
+  {
+    if (af = AF_INET) {
+      printf("Custom lookup\n");
+
+      strcpy(buffer, "nsstest.varoun.com");
+      host->h_name = buffer;
+      host->h_aliases = NULL;
+      host->h_addrtype = AF_INET;
+      host->h_length = 4;
+
+      char *hostaddrp;
+      hostaddrp = buffer + 256;
+      int hostaddr = inet_addr("1.2.3.4");
+      char *offset = memcpy(hostaddrp, &hostaddr, 4);
+      host->h_addr_list[0] = NULL;
+      return NSS_STATUS_SUCCESS;
+    }
+  }
+
+
   return NSS_STATUS_NOTFOUND;
 };
 
