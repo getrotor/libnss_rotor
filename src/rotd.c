c8b4110900c0eb564a2bc8a1b74ed59a0c1e6503
Varoun. P <contact@varoun.com>
2015-09-12 01:36:00 -0400

    Fix segfault issues, the correct address is currently NOT returned.

--- src/rotd.c
+++ src/rotd.c
@@ -5,10 +5,11 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+#define ALIGN(a) (((a+sizeof(void*)-1)/sizeof(void*))*sizeof(void*))
 /*
 NOTE(varoun): Look at include/netdb.h in the glibc source for the
 NSS Prototypes.
- */
+*/
 
 enum nss_status
 _nss_rotd_sethostent (int flag) {
@@ -35,25 +36,45 @@ _nss_rotd_gethostbyname2_r (const char *name, int af, struct hostent *host,
                             char *buffer, size_t buflen,
                             int *errnop, int *h_errnop) {
   printf("@ %s\n", __FUNCTION__);
-  printf("ARG(name) -> %s\n", name);
-  printf("ARG(buflen) -> %d\n", buflen);
+
+  size_t l, idx, alen;
+  char *r_addr, *r_name, *r_aliases, *r_addr_list;
 
   if (strcmp(name, "nsstest.varoun.com") == 0)
   {
     if (af = AF_INET) {
       printf("Custom lookup\n");
 
-      strcpy(buffer, "nsstest.varoun.com");
-      host->h_name = buffer;
-      host->h_aliases = NULL;
-      host->h_addrtype = AF_INET;
-      host->h_length = 4;
+      /* First fill in the hostname */
+      l = strlen(name);
+      r_name = buffer;
+      memcpy(r_name, name, l+1);
+      idx = ALIGN(l+1);
 
-      char *hostaddrp;
-      hostaddrp = buffer + 256;
-      int hostaddr = inet_addr("1.2.3.4");
-      char *offset = memcpy(hostaddrp, &hostaddr, 4);
-      host->h_addr_list[0] = NULL;
+      /* Second, create  an empty aliases array */
+      r_aliases = buffer + idx;
+      *(char**) r_aliases = NULL;
+      idx += sizeof(char*);
+
+      /* Third, add the address */
+      in_addr_t addr = inet_addr("1.2.3.4");
+      alen = 4; /* 4 for AF_INET */
+
+      memcpy(r_addr, &addr, alen);
+        idx += ALIGN(alen);
+
+      /* Fourth, add address pointer array */
+      r_addr_list = buffer + idx;
+      ((char**) r_addr_list)[0] = r_addr;
+      ((char**) r_addr_list)[1] = NULL;
+
+
+      host->h_name = r_name;
+      host->h_aliases = (char**) r_aliases;
+      host->h_addrtype = af;
+      host->h_length = alen;
+      host->h_addr_list = (char**) r_addr_list;
+
       return NSS_STATUS_SUCCESS;
     }
   }
