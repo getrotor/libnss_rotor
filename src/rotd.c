385236261103d44d4dde18a07754681bcac41194
Varoun. P <contact@varoun.com>
2015-09-15 09:31:40 -0400

    Move filling in the hostent data to a separate function.

--- src/rotd.c
+++ src/rotd.c
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <assert.h>
+#include <errno.h>
 
 #define _public_ __attribute__ ((visibility("default")))
 #define _hidden_ __attribute__ ((visibility("hidden")))
@@ -67,6 +68,77 @@ _nss_rotd_gethostbyaddr_r(const void* addr, socklen_t len,
                                 char *buffer, size_t buflen,
                                 int *errnop, int *h_errnop) _public_;
 
+
+
+static enum nss_status
+fill_in_hostent(const char *name, int af,
+                struct hostent *result,
+                char *buffer, size_t buflen,
+                int *errnop, int *h_errnop,
+                int32_t *ttlp,
+                char **canonp) {
+
+  size_t l, idx, ms;
+  char *r_addr, *r_name, *r_aliases, *r_addr_list;
+
+  size_t alen = PROTO_ADDRESS_SIZE(af);
+
+  /* Make the query to rotor and get the result */
+  char real[16];
+  get_real(name, real);
+
+  l = strlen(name);
+  ms = ALIGN(l+1) +
+    sizeof(char*)+
+    ALIGN(alen)+
+    2*sizeof(char*);
+
+  if (buflen < ms) {
+    *errnop = ENOMEM;
+    *h_errnop = NO_RECOVERY;
+    return NSS_STATUS_TRYAGAIN;
+  }
+
+  /* First fill in the hostname */
+  r_name = buffer;
+  memcpy(r_name, name, l+1);
+  idx = ALIGN(l+1);
+
+  /* Second - Create an empty aliases array */
+  r_aliases = buffer + idx;
+  *(char**) r_aliases = NULL;
+  idx += sizeof(char*);
+
+  /* Third - add the addresses */
+  r_addr = buffer + idx;
+  in_addr_t addr = inet_addr(real);
+  memcpy(r_addr, &addr, alen);
+  idx += ALIGN(alen);
+
+  /* Fourth - Add address pointer array */
+
+  r_addr_list = buffer + idx;
+  ((char**) r_addr_list)[0] = r_addr;
+  ((char**) r_addr_list)[1] = NULL;
+  idx += 2*sizeof(char*);
+
+  /* Verify that the size matches */
+  assert(idx = ms);
+
+  result->h_name = r_name;
+  result->h_aliases = (char**) r_aliases;
+  result->h_addrtype = af;
+  result->h_length = alen;
+  result->h_addr_list = (char**) r_addr_list;
+
+  if (ttlp)
+    *ttlp = 0;
+
+  if (canonp)
+    *canonp = r_name;
+
+  return NSS_STATUS_SUCCESS;
+}
 
 
 enum nss_status
